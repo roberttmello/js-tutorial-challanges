20b7137f634c1dac179e3ccc5c5153679fb83592
roberttmello <roberttmello@protonmail.com>
2022-10-17 16:46:46 -0300

    First commit

---
+++ challenges/02-concat.js
@@ -0,0 +1,16 @@
+/*
+## Concatenate Strings #2
+
+1. create "street" and "country" variables
+2. assign your values
+3. create "fullMailingAddress" variable and assign
+   the result of "street + country"
+4. remember about the space
+5. log "fullMailingAddress" in the console
+*/
+
+let street = 'Guitar street'
+let country = 'Brazil'
+
+const fullMailingAddress = `${street} ${country}`
+console.log(fullMailingAddress)
